647a89490164a8e09b1ce9f0b21e9fa64fde45db
Akoudimati <143807532+Akoudimati@users.noreply.github.com>
2023-12-04 12:57:16 +0100

    .

--- pro_js/main.js
+++ pro_js/main.js
@@ -1,21 +1,21 @@
-
-document.getElementById('maze_game').addEventListener('click', function() {
+document.querySelector('.escaperoom').addEventListener('click', function () {
+    window.location.href = '../escaperoom/index.html';
+  });
+  document.querySelector('.maze_game').addEventListener('click', function () {
     window.location.href = '../maze-game/index.html';
-});
-
-document.getElementById('boodschappen2').addEventListener('click', function() {
+  });
+  document.querySelector('.boodschappen2').addEventListener('click', function () {
     window.location.href = '../boodsc/index.html';
-});
-document.getElementById('pokedex').addEventListener('click', function() {
-    window.location.href = '../challenge-pokedex-js/index.html';
-});
-document.getElementById('hoger-lager').addEventListener('click', function() {
+  });
+  document.querySelector('.hoger-lager').addEventListener('click', function () {
     window.location.href = '../hoger_lager/index.html';
-});
-document.getElementById('whack-a-mole').addEventListener('click', function() {
+  });
+  document.querySelector('.whack-a-mole').addEventListener('click', function () {
     window.location.href = '../koudimati/index.html';
-});
-document.getElementById('build-a-game').addEventListener('click', function() {
-    window.location.href = './koudimati/index.html';
-});
-
+  });
+  document.querySelector('.pokedex').addEventListener('click', function () {
+    window.location.href = '../challenge-pokedex-js/index.html';
+  });
+  document.querySelector('escaperoom').addEventListener('click', function () {
+    window.location.href = '../escaperoom/index.html';
+  });
