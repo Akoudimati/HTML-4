7346d2cdda01c4178e9c27eec94b1be45f0e9e7f
Akoudimati <143807532+Akoudimati@users.noreply.github.com>
2024-06-10 13:48:00 +0200

    js fix

--- pro_js/main.js
+++ pro_js/main.js
@@ -1,19 +1,19 @@
-document.querySelector('.escaperoom').addEventListener('click', function () {
-    window.location.href = '../escaperoom/index.html';
-  });
-  document.querySelector('.maze_game').addEventListener('click', function () {
-    window.location.href = '../maze-game/index.html';
-  });
-  document.querySelector('.boodschappen2').addEventListener('click', function () {
-    window.location.href = '../boodsc/index.html';
-  });
-  document.querySelector('.hoger-lager').addEventListener('click', function () {
-    window.location.href = '../hoger_lager/index.html';
-  });
-  document.querySelector('.whack-a-mole').addEventListener('click', function () {
-    window.location.href = '../koudimati/index.html';
-  });
-  document.querySelector('.pokedex').addEventListener('click', function () {
-    window.location.href = '../challenge-pokedex-js/index.html';
-  });
+// document.querySelector('.escaperoom').addEventListener('click', function () {
+//     window.location.href = '../escaperoom/index.html';
+//   });
+//   document.querySelector('.maze_game').addEventListener('click', function () {
+//     window.location.href = '../maze-game/index.html';
+//   });
+//   document.querySelector('.boodschappen2').addEventListener('click', function () {
+//     window.location.href = '../boodsc/index.html';
+//   });
+//   document.querySelector('.hoger-lager').addEventListener('click', function () {
+//     window.location.href = '../hoger_lager/index.html';
+//   });
+//   document.querySelector('.whack-a-mole').addEventListener('click', function () {
+//     window.location.href = '../koudimati/index.html';
+//   });
+//   document.querySelector('.pokedex').addEventListener('click', function () {
+//     window.location.href = '../challenge-pokedex-js/index.html';
+//   });
 
